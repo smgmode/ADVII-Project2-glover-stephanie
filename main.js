3d39a617e91a469584103e3359c12621a1d5bc0f
Stephanie Glover <stephanie.glover@ufl.edu>
2019-03-26 00:02:52 -0700

    Made last minute changes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//extra paragraph
+//extra paragraph first button
 var first = document.getElementById ("firstbutton"),
     epara = document.getElementById ("extrapara");
  firstbutton.addEventListener("click", function (event) {
@@ -7,3 +7,23 @@ var first = document.getElementById ("firstbutton"),
   "none";
 }, false);
   epara.style.display = "none";
+
+  //extra paragraph second button
+  var second = document.getElementById ("secondbutton"),
+      eparaII = document.getElementById ("extraparaII");
+   secondbutton.addEventListener("click", function (event) {
+    event.preventDefault ();
+    eparaII.style.display = (eparaII.style.display === "none") ? "block" :
+    "none";
+  }, false);
+    eparaII.style.display = "none";
+
+    //extra paragraph third button
+    var third = document.getElementById ("thirdbutton"),
+        eparaIII = document.getElementById ("extraparaIII");
+     thirdbutton.addEventListener("click", function (event) {
+      event.preventDefault ();
+      eparaIII.style.display = (eparaIII.style.display === "none") ? "block" :
+      "none";
+    }, false);
+      eparaIII.style.display = "none";
